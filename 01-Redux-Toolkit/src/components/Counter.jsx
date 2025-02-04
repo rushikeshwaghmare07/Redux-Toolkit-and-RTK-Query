3a0e05044334b37d77700da48ac9b0ea756a510f
Rushikesh Ramesh Waghmare <rushikeshwaghmare862@gmail.com>
2025-02-05 00:16:19 +0530

    Implement counter functionality using redux store

---
+++ 01-Redux-Toolkit/src/components/Counter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../features/counter/counterSlice";
+
+const Counter = () => {
+  // Read the data from the "store"
+  const count = useSelector((state) => state.counter.value);
+
+  // Changing the data by sending "actions" to the store
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  )
+}
+
+export default Counter
